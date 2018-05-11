aa7ed915b422e4aaa07fa7c9d99e3864868aad43
cindy <1689456277@qq.com>
2018-05-11 18:23:17 +0800

    0511

---
+++ src/components/music-list/music-list.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="music-list">
+    <div class="back"><i class="icon-back"></i></div>
+    <h1 class="title" v-html="title"></h1>
+    <div class="bg-image" :style="bgStyle" ref="bgImage"></div>
+    <scroll class="list" ref="list" :probeType="probeType" :listenScroll="listenScroll" @scroll="scroll">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
+    <div class="bg-layer" ref="bgLayer"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+
+  const RESERVE_HEIGHT = 40
+
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ''
+      },
+      bgImage: {
+        type: String,
+        default: ''
+      },
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    data() {
+      return {
+        scrollY: 0
+      }
+    },
+    created() {
+      this.probeType = 3
+      this.listenScroll = true
+    },
+    mounted() {
+      this.imgHeight = this.$refs.bgImage.clientHeight
+      this.$refs.list.$el.style.top = `${this.imgHeight}px`
+      this.minTranslateY = -this.imgHeight + RESERVE_HEIGHT
+    },
+    methods: {
+      scroll(pos){
+        this.scrollY = pos.y
+      }
+    },
+    watch: {
+      scrollY(newY){
+        let translateY = Math.max(this.minTranslateY, newY)
+        let zIndex = 0
+        let scale = 1
+        let blur = 1;
+        const percent = Math.abs(newY / this.imgHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.max(0.2, 1-percent)
+        }
+        this.$refs.bgLayer.style['transform'] = `translate3d(0, ${translateY}px,0)`
+        this.$refs.bgImage.style['opacity'] = `${blur}`
+        if(newY < translateY){
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVE_HEIGHT}px`
+          zIndex = 10
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+        }
+        this.$refs.bgImage.style['transform'] = `scale(${scale})`
+        this.$refs.bgImage.style.zIndex = zIndex
+      }
+    },
+    computed: {
+      bgStyle() {
+        return `background-image:url(${this.bgImage})`
+      }
+    },
+    components:{
+      Scroll,SongList
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .music-list
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    bottom: 0
+    right: 0
+    background: $color-background
+    .back
+      position absolute
+      top: 0
+      left: 6px
+      z-index: 50
+      .icon-back
+        display: block
+        padding: 10px
+        font-size: $font-size-large-x
+        color: $color-theme
+    .title
+      position: absolute
+      top: 0
+      left: 10%
+      z-index: 40
+      width: 80%
+      no-wrap()
+      text-align: center
+      line-height: 40px
+      font-size: $font-size-large
+      color: $color-text
+    .bg-image
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 70%
+      transform-origin: top
+      background-size: cover
+      .play-wrapper
+        position: absolute
+        bottom: 20px
+        z-index: 50
+        width: 100%
+        .play
+          box-sizing: border-box
+          width: 135px
+          padding: 7px 0
+          margin: 0 auto
+          text-align: center
+          border: 1px solid $color-theme
+          color: $color-theme
+          border-radius: 100px
+          font-size: 0
+          .icon-play
+            display: inline-block
+            vertical-align: middle
+            margin-right: 6px
+            font-size: $font-size-medium-x
+          .text
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-small
+      .filter
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: rgba(7, 17, 27, 0.4)
+    .bg-layer
+      position: relative
+      height: 100%
+      background: $color-background
+    .list
+      position: fixed
+      top: 0
+      bottom: 0
+      width: 100%
+      background: $color-background
+      z-index:1
+      .song-list-wrapper
+        padding: 20px 30px
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+</style>
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-
-  </div>
+  <transition name="slider">
+      <music-list :title="title" :bgImage="bgImage" :songs="songs"></music-list>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
   import {createSong} from 'common/js/song'
   import {getSingerDetail} from 'api/singer'
   import {mapGetters} from 'vuex'
@@ -18,6 +19,12 @@
     computed:{
       id(){
         return this.singer.id
+      },
+      title(){
+        return this.singer.name
+      },
+      bgImage(){
+        return this.singer.avatar
       },
       ...mapGetters([
         'singer'
@@ -44,14 +51,23 @@
         list.forEach((item) => {
           let {musicData} = item
           if(musicData.songid && musicData.albummid){
-            ret.push(createSong(musicData))
+              ret.push(createSong(musicData))
           }
         })
-        console.info(ret)
         return ret
       }
+    },
+    components: {
+      MusicList
     }
 
   }
 </script>
 
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slider-enter-active, .slider-live-active
+    transition:all .3s
+
+  .slider-enter,.slider-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
